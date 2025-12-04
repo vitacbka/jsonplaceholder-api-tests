a2118b706497b18fa0cdd1c122094f41a9b21677
vitaliy <vitacbka@gmail.com>
2025-12-04 12:40:33 +0300

    Add full CRUD and filtering tests for posts API

--- src/test/java/test/JsonPlaceHolderApiTests.java
+++ src/test/java/test/JsonPlaceHolderApiTests.java
@@ -1,9 +1,11 @@
 package test;
 
+import models.Post;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 public class JsonPlaceHolderApiTests extends BaseTest {
 
@@ -19,5 +21,92 @@ public class JsonPlaceHolderApiTests extends BaseTest {
                 .body("userId", equalTo(1))
                 .body("title", not(emptyString()))
                 .body("body", not(emptyString()));
+    }
+
+    @Test
+    public void testGetAllPostTest() {
+        given()
+                .spec(requestSpec)
+                .when()
+                .get("/posts")
+                .then()
+                .statusCode(200)
+                .body("$.size()", equalTo(100));
+    }
+
+    @Test
+    public void testCreatePost() {
+        Post newPost = Post.builder()
+                .title("foo")
+                .body("bar")
+                .userId(1)
+                .build();
+        given()
+                .spec(requestSpec)
+                .body(newPost)
+                .when()
+                .post("/posts")
+                .then()
+                .statusCode(201)
+                .body("id", equalTo(101))
+                .body("title", equalTo(newPost.getTitle()))
+                .body("body", equalTo(newPost.getBody()))
+                .body("userId", equalTo(newPost.getUserId()));
+    }
+
+    @Test
+    public void testUpdatePost() {
+        Post updatedPost = Post.builder()
+                .id(1)
+                .title("updated title")
+                .body("updated body")
+                .userId(1)
+                .build();
+
+        given()
+                .spec(requestSpec)
+                .body(updatedPost)
+                .when()
+                .put("/posts/1")
+                .then()
+                .statusCode(200)
+                .body("title", equalTo(updatedPost.getTitle()))
+                .body("body", equalTo(updatedPost.getBody()));
+    }
+
+    @Test
+    void patchPostTest() {
+        given()
+                .spec(requestSpec)
+                .when()
+                .delete("/posts/1")
+                .then()
+                .statusCode(200)
+                .body(is("{}"));
+    }
+
+    @Test
+        public void filterPostsByUserIdTest() {
+            given()
+                    .spec(requestSpec)
+                    .queryParam("userId", 1)
+                    .when()
+                    .get("/posts")
+                    .then()
+                    .statusCode(200)
+                    .body("$.size()", equalTo(10))
+                    .body("userId", everyItem(equalTo(1)));
+    }
+
+    @Test
+    void getCommentsForPostTest() {
+        given()
+                .spec(requestSpec)
+                .when()
+                .get("/posts/1/comments")
+                .then()
+                .statusCode(200)
+                .body("$.size()", greaterThan(0))
+                .body("[0].postId", equalTo(1));
     }
 }
